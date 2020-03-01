12736b00c31ef75a345c97aa8fd7e0173507d83b
dinesh-prajapati5033 <dineshprajapati5033@gmail.com>
2020-03-01 12:29:27 +0530

    demo changies 6

--- demo.php
+++ demo.php
@@ -3,9 +3,6 @@
 <?php echo "bs jalsa ho"; ?>
 <?php echo "tamare kem se"; ?>
 <?php echo "halo khav"; ?>
-<<<<<<< HEAD
-
 <?php echo "halo khav"; ?>
-=======
 <?php echo "halo tar khav"; ?>
->>>>>>> 0fc5b0841bbb64939741b1578f37bf2516ad5571
+
