70884b0ae0ee9d910abdd92ba942d0e4052a2070
dinesh-prajapati5033 <dineshprajapati5033@gmail.com>
2020-03-01 11:55:10 +0530

    demo update 3

--- demo.php
+++ demo.php
@@ -1,7 +1,5 @@
 <?php echo "hello dear"; ?>
 <?php echo "majam ho"; ?>
-<<<<<<< HEAD
 <?php echo "bs jalsa ho"; ?>
-=======
 <?php echo "tamare kem se"; ?>
->>>>>>> b1ba544460817f03824086a398ab5f04845b6630
+
