29874d68b71f80d9484c1c1a1916daf745dd7449
dinesh-prajapati5033 <dineshprajapati5033@gmail.com>
2020-03-08 11:07:49 +0530

    done

--- demo.php
+++ demo.php
@@ -1,11 +1,3 @@
 <?php echo "hello dear"; ?>
 <?php echo "majam ho"; ?>
-<?php echo "bs jalsa ho"; ?>
-<?php echo "tamare kem se"; ?>
-<?php echo "halo khav"; ?>
-<?php echo "halo khav"; ?>
-<?php echo "halo tar khav"; ?>
-<?php echo "halo khav"; ?>
-<?php echo "halo tar khav"; ?>
-<?php echo "kem alya"; ?>
 <?php echo "hello dear"; ?>
